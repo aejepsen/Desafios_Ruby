339be416f3e85e601f7fa7143b78017fbc6a859e
allan <aejepsen@yahoo.com.br>
2022-10-06 21:45:12 -0300

    array-for-each

--- cookbook_array_for.rb
+++ cookbook_array_for.rb
@@ -2,11 +2,14 @@ puts "Bem-vindo as Cookbook, sua rede social de receitas!"
 receitas = []
 while(true) do
     puts "Digite a receita"
-    receita = gets.chomp
-    receitas << receita
+    nome_receita = gets.chomp
+    receitas << nome_receita
     puts "Receita cadastrada com sucesso!"
     for receita in receitas do
-        puts "Receita: #{receitas}"
+        puts "Receita_for: #{receita}"
      end
+    receitas.each do |receita|
+        puts "Receita_each: #{receita}"
+    end
     puts "Obrigado por utilizar o Cookbook!"
 end
